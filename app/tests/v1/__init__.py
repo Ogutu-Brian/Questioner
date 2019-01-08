dbcfac89a641885ab4ada0c934e8bd8c04f7f812
Ogutu Brian <codingbrian58@gmail.com>
2019-01-08 19:46:03 +0300

    [Feature #163030960] Added test for valid sign up

--- app/tests/v1/__init__.py
+++ app/tests/v1/__init__.py
@@ -1,5 +1,6 @@
 from app.run import app
 import pytest
+from app.api.v1.views import db
 
 app.config['TESTING'] = True
 
@@ -9,11 +10,17 @@ def client():
     return client
 
 
-valid_user_data = {
+user_data = {
     "headers": {
         "Content-Type": "application/json"
     },
-    "data": {
-        
-    }
+    "sign_up": {
+        "firstname": "Ogutu",
+        "lastname": "Brian",
+        "othername": "Okinyi",
+        "phoneNumber": "0703812914",
+        "username": "Brian",
+        "password": "password"
+    },
+    "sign_up_url": "/api/v1/users/sign-up"
 }
--- app/tests/v1/test_user.py
+++ app/tests/v1/test_user.py
@@ -1,8 +1,27 @@
 import unittest
+from .import client, user_data
+import json
 
 
 class TestUser(unittest.TestCase):
     """A class for testing user endpoints and operations"""
 
-    def test_sign_up(self):
-        pass
+    def sign_up(self, url, data={}, headers={}):
+        """
+        user sign up method by test client
+        """
+        result = client().post(url, data=json.dumps(data), headers=headers)
+        return json.loads(result.get_data(as_text=True))
+
+    def login(self, url, data={}, headers={}):
+        """A method for logging in by test client"""
+        result = client().post(url, data=json.dumps(data), headers=headers)
+        return json.loads(result.get_dta(as_text=True))
+
+    def test_valid_sign_up(self):
+        """tests for a valid user sign up given correct data"""
+        headers = user_data.get("headers")
+        data = user_data.get("sign_up")
+        url = user_data.get("sign_up_url")
+        result = self.sign_up(url=url, data=data, headers=headers)
+        self.assertEqual("success",result.get("status"))
