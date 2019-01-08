ab431de4759fb6c225e618b7963ea4b69d9535e8
Ogutu Brian <codingbrian58@gmail.com>
2019-01-08 19:14:15 +0300

    [Feature #163030960] updated test package

--- app/tests/v1/__init__.py
+++ app/tests/v1/__init__.py
@@ -1,6 +1,19 @@
 from app.run import app
+import pytest
+
+app.config['TESTING'] = True
 
 
 def client():
     client = app.test_client()
     return client
+
+
+valid_user_data = {
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "data": {
+        
+    }
+}
--- app/tests/v1/test_user.py
+++ app/tests/v1/test_user.py
@@ -3,5 +3,6 @@ import unittest
 
 class TestUser(unittest.TestCase):
     """A class for testing user endpoints and operations"""
+
     def test_sign_up(self):
         pass
