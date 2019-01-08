36aa485a167523e730fd840880b3192745f42ced
Ogutu Brian <codingbrian58@gmail.com>
2019-01-08 03:25:13 +0300

    [Chore #163032022] Added meetup model

--- app/api/v1/models/basemodel.py
+++ app/api/v1/models/basemodel.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 
-class BaseModel:
+class BaseModel(object):
     """Contains properties shared accorss all the models"""
 
     def __init__(self, id_=-1, created_on=""):
@@ -9,7 +9,8 @@ class BaseModel:
         self.created_on = created_on
 
     def to_dictionary(self)->Dict:
-        """Method to be overriden by child classes to convert objects to dictionary
+        """Method to be overriden by child classes to return object properties in to dictionary
+        format
         """
         return {
             "id": self.id,
@@ -17,5 +18,7 @@ class BaseModel:
         }
 
     def created_date(self):
-        """Reeturns date created as a formatted string"""
-        return self.created_on.strftime('%d-%m-%Y')
+        """Reeturns date created as a formatted string (day-month-year) if date is provided"""
+        if self.created_on:
+            return self.created_on.strftime('%d-%m-%Y')
+        return self.created_date
--- app/api/v1/models/usermodel.py
+++ app/api/v1/models/usermodel.py
@@ -6,8 +6,8 @@ from typing import Dict
 class User(BaseModel):
     """A model for user information"""
 
-    def __init__(self, id_, created_on=datetime.date.today(), first_name="", last_name="",
-                 other_name="", email="", phone_number="", user_name="", registred="", is_admin=False):
+    def __init__(self, id_=-1, created_on=datetime.date.today(), first_name="", last_name="",
+                 other_name="", email="", phone_number="", user_name="", registred="", is_admin=False, password=""):
         super().__init__(id_=id_, created_on=created_on)
         self.first_name = first_name
         self.last_name = last_name
@@ -17,9 +17,10 @@ class User(BaseModel):
         self.user_name = user_name
         self.registred = registred
         self.is_admin = is_admin
+        self.password = password
 
     def to_dictionary(self)->Dict:
-        """Overrides the basemodels to dictionary method"""
+        """Overrides the basemodel's method to present user object data in dictionary format"""
         if self.registred:
             self.registred = self.registred.strftime('%d-%m-%Y')
         dict_data = {
