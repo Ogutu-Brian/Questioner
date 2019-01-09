232dc8ec9af84002b1a769a77215611c718feea3
Ogutu Brian <codingbrian58@gmail.com>
2019-01-09 20:23:28 +0300

    [Feature #163026887] Added test for non json object

--- app/tests/v1/test_questions.py
+++ app/tests/v1/test_questions.py
@@ -11,6 +11,7 @@ class TestQuestion(unittest.TestCase):
         """Creates a user and a meetup and creartes questions using those details"""
         data = json.dumps(data)
         result = client().post(url, data=data, headers=headers)
+        db.tear_down()
         return json.loads(result.get_data(as_text=True))
 
     def test_correct_question_post(self):
@@ -18,3 +19,22 @@ class TestQuestion(unittest.TestCase):
         result = self.create_question(url=question_data.get(
             "url"), data=question_data.get("data"), headers=question_data.get("headers"))
         self.assertEqual(status.created, result.get("status"))
+
+    def test_non_json_data(self):
+        question_data = {
+            "headers": {
+                # "Content-Type": "application/json"
+            },
+            "data": {
+                "title": "Responnsive Web design",
+                "createdBy": 1,
+                "response": "yes",
+                "body": "What is the best way of getting around responsiveness of a website",
+                "meetup": 3
+            },
+            "url": "/api/v1/questions"
+        }
+        result = self.create_question(url=question_data.get(
+            "url"), data=question_data.get("data"), headers=question_data.get("headers"))
+        print(result)
+        self.assertEqual(status.not_json, result.get("status"))
