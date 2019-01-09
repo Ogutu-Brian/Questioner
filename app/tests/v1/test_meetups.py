9a9aeb14472e62dcaff708ae86d1c3a678ddcbdf
Ogutu Brian <codingbrian58@gmail.com>
2019-01-09 22:51:01 +0300

    [Starts #163028936] Added tests for get upcoming meetups endpoint

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -140,7 +140,19 @@ class TestMeetup(unittest.TestCase):
         self.assertEqual(status.created, meetup.get("status"))
         meetup_id = meetup.get("data")[0].get("id")
         get_url = "/api/v1/meetups/{}".format(meetup_id)
-        print(get_url)
         result = json.loads(client().get(get_url).get_data(as_text=True))
         self.assertEqual(status.success, result.get("status"))
         db.tear_down()
+
+    def test_get_all_meetup_records(self):
+        """"Test for getting all meetups endoint"""
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        meetup1 = self.create_meetup(url=url, data=data, headers=headers)
+        self.assertEqual(status.created, meetup1.get("status"))
+        meetup2 = self.create_meetup(url=url, data=data, headers=headers)
+        self.assertEqual(status.created, meetup2.get("status"))
+        get_url = "/api/v1/meetups/upcoming/"
+        result = json.loads(client().get(get_url).get_data(as_text=True))
+        self.assertEqual(status.success, result.get("status"))
