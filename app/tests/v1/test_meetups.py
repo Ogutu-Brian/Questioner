4de0d6b07f715d9dabedfc6e4133644c7a176ddc
Ogutu Brian <codingbrian58@gmail.com>
2019-01-09 09:25:40 +0300

    [Finishes #163026690] Added more tests

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -18,4 +18,112 @@ class TestMeetup(unittest.TestCase):
         headers = meetup_data.get("headers")
         url = meetup_data.get("url")
         result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
         self.assertEqual(status.created, result.get("status"))
+
+    def test_missing_tags(self):
+        meetup_data = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": {
+                "location": "Andela Campus",
+                "images": ["/images/important", "/images/meetup"],
+                "topic": "Responsive Web Design",
+                # "Tags": ["User Interface", "Responsive Design"],
+                "happeningOn": "2018-04-23T18:25:43.511Z"
+            },
+            "url": "/api/v1/meetups"
+        }
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
+        self.assertEqual(status.invalid_data, result.get("status"))
+
+    def test_missing_location(self):
+        meetup_data = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": {
+                # "location": "Andela Campus",
+                "images": ["/images/important", "/images/meetup"],
+                "topic": "Responsive Web Design",
+                "Tags": ["User Interface", "Responsive Design"],
+                "happeningOn": "2018-04-23T18:25:43.511Z"
+            },
+            "url": "/api/v1/meetups"
+        }
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
+        self.assertEqual(status.invalid_data, result.get("status"))
+
+    def test_missing_meetup_date(self):
+        meetup_data = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": {
+                "location": "Andela Campus",
+                "images": ["/images/important", "/images/meetup"],
+                "topic": "Responsive Web Design",
+                "Tags": ["User Interface", "Responsive Design"],
+                # "happeningOn": "2018-04-23T18:25:43.511Z"
+            },
+            "url": "/api/v1/meetups"
+        }
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
+        self.assertEqual(status.invalid_data, result.get("status"))
+
+    def test_missing_images(self):
+        """Image location is optional so missing in json object should not cause failure"""
+        meetup_data = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": {
+                "location": "Andela Campus",
+                # "images": ["/images/important", "/images/meetup"],
+                "topic": "Responsive Web Design",
+                "Tags": ["User Interface", "Responsive Design"],
+                "happeningOn": "2018-04-23T18:25:43.511Z"
+            },
+            "url": "/api/v1/meetups"
+        }
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
+        self.assertEqual(status.created, result.get("status"))
+
+    def test_invalid_post_object(self):
+        """Tests if the data being posted is actually json"""
+        meetup_data = {
+            "headers": {
+                # "Content-Type": "application/json"
+            },
+            "data": {
+                "location": "Andela Campus",
+                "images": ["/images/important", "/images/meetup"],
+                "topic": "Responsive Web Design",
+                "Tags": ["User Interface", "Responsive Design"],
+                "happeningOn": "2018-04-23T18:25:43.511Z"
+            },
+            "url": "/api/v1/meetups"
+        }
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        result = self.create_meetup(url=url, data=data, headers=headers)
+        db.tear_down()
+        self.assertEqual(status.not_json, result.get("status"))
