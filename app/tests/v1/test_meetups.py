b696409fde5ada36e2d3d189b5be012e092511ff
Ogutu Brian <codingbrian58@gmail.com>
2019-01-10 03:22:58 +0300

    [Feature #163030657] Added test for invalid user id

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -194,7 +194,6 @@ class TestMeetup(unittest.TestCase):
         }
         result = json.loads(client().post(rsvp_data.get("url"), data=json.dumps(
             rsvp_data.get("data")), headers=headers).get_data(as_text=True))
-        print(result)
         self.assertEqual(status.created, result.get("status"))
         db.tear_down()
 
@@ -221,6 +220,32 @@ class TestMeetup(unittest.TestCase):
         }
         result = json.loads(client().post(rsvp_data.get("url"), data=json.dumps(
             rsvp_data.get("data")), headers=headers).get_data(as_text=True))
-        print(result)
         self.assertEqual(status.not_found, result.get("status"))
+        db.tear_down()
+
+    def test_ivalid_user_for_rsvp(self):
+        """Tests for invalid user creating the rsvp"""
+        data = meetup_data.get("data")
+        headers = meetup_data.get("headers")
+        url = meetup_data.get("url")
+        meetup = self.create_meetup(url=url, data=data, headers=headers)
+        self.assertEqual(status.created, meetup.get("status"))
+        meetup_id = meetup.get("data")[0].get("id")
+        data = user_data.get("sign_up")
+        url = user_data.get("sign_up_url")
+        # user = self.create_user(url=url, data=data, headers=headers)
+        # user_id = user.get("data")[0].get("id")
+        # self.assertEqual(status.created, user.get("status"))
+        user_id = 0
+        rsvp_data = {
+            "data": {
+                "user": user_id,
+                "response": "yes"
+            },
+            "url": "/api/v1/meetups/{}/rsvps".format(meetup_id)
+        }
+        result = json.loads(client().post(rsvp_data.get("url"), data=json.dumps(
+            rsvp_data.get("data")), headers=headers).get_data(as_text=True))
+        print(result)
+        self.assertEqual(status.invalid_data, result.get("status"))
         db.tear_down()
