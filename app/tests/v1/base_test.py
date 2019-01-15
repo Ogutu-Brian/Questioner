7b16965cdf9e2bc18b3479b3f70c2a5914db13ac
Ogutu Brian <codingbrian58@gmail.com>
2019-01-16 00:14:45 +0300

    [Finishes #163237565] Add docstring to basetest

--- app/tests/v1/base_test.py
+++ app/tests/v1/base_test.py
@@ -6,8 +6,10 @@ class BaseTest(unittest.TestCase):
     app.config["TESTING"] = True
 
     def setUp(self):
+        """Set up of application test client"""
         self.app = app
         self.client = self.app.test_client
 
     def tearDown(self):
+        """Clears my db data"""
         db.tear_down()
