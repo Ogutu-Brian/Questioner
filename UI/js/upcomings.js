6581efbec5cee4f1e0808afc342c7d8c1c052f32
Ogutu Brian <codingbrian58@gmail.com>
2019-02-03 20:20:06 +0300

    [Feature #163680803] Add javascript to load upcoming meetups from database

---
+++ UI/js/upcomings.js
@@ -0,0 +1,22 @@
+let meetupUrl = 'http://127.0.0.1:5000/api/v2/meetups/upcoming/';
+window.onload = fetch(meetupUrl, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(response => response.json())
+    .then(data => {
+        let result = ''
+        for (let item of data.data) {
+            result +=
+                `<div class="card" onclick="location.href='questions.html'">
+                    <div class="card-info">
+                        <p>${item.topic}</p>
+                        <p class="location">${item.location}</p>
+                        <p class="date">${item.happeningOn}</p>
+                        <p class="info">${item.body}</p>
+                    </div>
+                </div>`;
+        }
+        document.getElementById('result').innerHTML = result;
+    })
