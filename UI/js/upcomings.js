080776b8e996ced1c2d0c3fb828ea0f9623fb0c1
Ogutu Brian <codingbrian58@gmail.com>
2019-02-04 11:52:45 +0300

    [Feature #163687549] Add page redirection to questions

--- UI/js/upcomings.js
+++ UI/js/upcomings.js
@@ -10,20 +10,21 @@ window.onload = fetch(meetupUrl, {
         for (let item of data.data) {
             result +=
                 `<div class="card" id=${item.id}>
-                    <div class="card-info">
-                        <p>${item.topic}</p>
-                        <p class="location">${item.location}</p>
-                        <p class="date">${item.happeningOn}</p>
-                        <p class="info">${item.body}</p>
+                    <div class="card-info" id=${item.id}>
+                        <p id=${item.id}>${item.topic}</p>
+                        <p class="location" id=${item.id}>${item.location}</p>
+                        <p class="date" id=${item.id}>${item.happeningOn}</p>
+                        <p class="info" id=${item.id}>${item.body}</p>
                     </div>
                 </div>`;
         }
         document.getElementById('result').innerHTML = result;
         for (let item of data.data) {
             document.getElementById(item.id).addEventListener('click', questions);
-            var questions = (event) => {
-                event.preventDefault();
-                localStorage.setItem("meetupId", item.id);
-            }
         }
     })
+var questions = (event) => {
+    //function that gets meetup id and sets it to local storage
+    localStorage.setItem("meetupId", event.target.id);
+    window.location.href = 'questions.html';
+}
