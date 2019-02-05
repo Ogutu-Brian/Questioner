ee0347f896538c81f45d595c67ace3a8a64dc6c8
Ogutu Brian <codingbrian58@gmail.com>
2019-02-05 12:16:58 +0300

    [Finishes #163708150] Write js code for posting comment

--- UI/js/comment.js
+++ UI/js/comment.js
@@ -14,6 +14,7 @@ class FormHandler {
     createFormData() {
         for (let name of this.fieldNames) {
             this.data[name] = this.getFieldValue(name);
+            this.data['question'] = localStorage.getItem('questionId');
         }
     }
     //gets the data being posted
@@ -57,8 +58,43 @@ window.onload = fetch(questionUrl, {
                     }
 
                 }
-                result += `<textarea name="" id="" cols="30" rows="10"></textarea><br>
-                <button class="comment-btn">Post Your Comment</button>`
+                result += `<textarea name="" id='comment' cols="30" rows="10"></textarea><br>
+                <button class="comment-btn" id='postComment'>Post Your Comment</button>`
                 document.getElementById('result').innerHTML = result;
+                let newCommentUrl = 'http://127.0.0.1:5000/api/v2/comments/';
+                let postButton = 'postComment';
+                let fieldNames = [
+                    'comment'
+                ];
+                document.getElementById(postButton).addEventListener('click', postComment);
+                let postCommentHandler = new FormHandler(newCommentUrl, fieldNames);
+                function postComment(event) {
+                    event.preventDefault();
+                    let authToken = 'Bearer ' + localStorage.getItem('token');
+                    fetch(postCommentHandler.getUrl, {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': authToken,
+                            'Content-Type': 'application/json'
+                        },
+                        body: postCommentHandler.Data
+                    }).then(newCommentResponse => newCommentResponse.json())
+                        .then(newCommentData => {
+                            if (newCommentData.status == 201) {
+                                window.location = window.location;
+                            } else if (newCommentData.status == 406) {
+                                window.alert(newCommentData.error[0].message);
+                            } else if (newCommentData.status == 401) {
+                                if (newCommentData.error[0].message == 'Your token has expired') {
+                                    window.alert("Your session has expired, please log in");
+                                    window.location.href = '../user/login.html';
+                                }
+                                else if (newCommentData.error[0].message == 'Token Bearer not given') {
+                                    window.alert("Please sign in in order to post a comment");
+                                    window.location.href = '../user/login.html';
+                                }
+                            }
+                        })
+                }
             })
     })
