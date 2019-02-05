d28da212444a5b27c7c2e0e6bea3e9a01c619972
Ogutu Brian <codingbrian58@gmail.com>
2019-02-05 14:37:15 +0300

    [Feature #163708150] Fix bug in comments button

--- UI/js/questions.js
+++ UI/js/questions.js
@@ -26,16 +26,22 @@ window.onload = fetch(questionsUrl, {
                     <button class="fa fa-thumbs-up button button-margin"> 0</button>
                 </td>
                 <td>
-                    <button class="fas fa-comments" onclick="location.href='questionview.html'"> ${item.comments}</button></td>
+                    <button class="fas fa-comments" id=${'question' + item.id}> ${item.comments}</button></td>
             </tr>`
             }
             document.getElementById('result').innerHTML = result;
             for (let item of data.data) {
                 document.getElementById(item.id).addEventListener('click', comments);
+                document.getElementById('question' + item.id).addEventListener('click', commentButton);
             }
         }
     })
 var comments = (event) => {
     localStorage.setItem('questionId', event.target.id);
+    window.location.href = '../user/questionview.html';
+}
+var commentButton = (event) => {
+    console.log(event.target.id.slice(8))
+    localStorage.setItem('questionId', event.target.id.slice(8));
     window.location.href = '../user/questionview.html';
 }
