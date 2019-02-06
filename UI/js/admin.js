eea9f5d53f59febd7cb1d7e9346ba8bff542911d
Ogutu Brian <codingbrian58@gmail.com>
2019-02-06 14:58:22 +0300

    [Feature #163740001] Add click event to meetups on admin panel

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -10,7 +10,7 @@ window.onload = fetch(meetupUrl, {
         if (data.status == 200) {
             let result = ''
             for (let item of data.data) {
-                result +=`<div class="meetup-card"=id=${item.id}>
+                result += `<div class="meetup-card"=id=${item.id}>
                 <div class="meetup-info" id=${item.id}>
                 <p id=${item.id}>${item.topic}</p>
                 <p class="location" id=${item.id}>${item.location}</p>
@@ -39,13 +39,13 @@ window.onload = fetch(meetupUrl, {
             </div>`
             }
             document.getElementById('result').innerHTML = result;
-            // for (let item of data.data) {
-            //     document.getElementById(item.id).addEventListener('click', questions);
-            // }
+            for (let item of data.data) {
+                document.getElementById(item.id).addEventListener('click', questions);
+            }
         }
     })
-// var questions = (event) => {
-//     //function that gets meetup id and sets it to local storage
-//     localStorage.setItem("meetupId", event.target.id);
-//     window.location.href = 'questions.html';
-// }
+var questions = (event) => {
+    //function that gets meetup id and sets it to local storage
+    localStorage.setItem("meetupId", event.target.id);
+    window.location.href = '../user/questions.html';
+}
