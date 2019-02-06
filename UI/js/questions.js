3e6b67c2d20e92dc9680448c52f7b6f254dc5564
Ogutu Brian <codingbrian58@gmail.com>
2019-02-06 23:32:29 +0300

    [Feature #163749529] Add form handler class

--- UI/js/questions.js
+++ UI/js/questions.js
@@ -2,6 +2,32 @@
 let meetupId = localStorage.getItem("meetupId");
 let questionsUrl = `http://127.0.0.1:5000/api/v2/questions/${meetupId}/`;
 let authToken = 'Bearer ' + localStorage.getItem('token');
+//Class that handles forms on click of buttons
+class FormHandler {
+    constructor(url, fieldNames) {
+        this.data = {};
+        this.url = url;
+        this.fieldNames = fieldNames;
+    }
+    //gets url
+    get getUrl() {
+        return this.url;
+    }
+    //creates the object to be posted
+    createFormData() {
+        for (let name of this.fieldNames) {
+            this.data[name] = this.getFieldValue(name);
+        }
+    }
+    //gets the data being posted
+    get Data() {
+        this.createFormData()
+        return JSON.stringify(this.data);
+    }
+    getFieldValue(name) {
+        return document.getElementById(name).value;
+    }
+}
 window.onload = fetch(questionsUrl, {
     method: 'GET',
     headers: {
